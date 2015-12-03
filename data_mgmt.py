4527bc18797ce3a698f82add5452d43131c33449
Sumit Gogia <summit@mit.edu>
2015-12-03 01:32:40 -0500

    fixed corpus loading into convenient Python data structures

--- data_mgmt.py
+++ data_mgmt.py
@@ -10,4 +10,17 @@ def load_model_params(load_file):
 
 def load_corpus(corpus_file):
     corpus = sio.loadmat(load_file)
-    return corpus
+    docmats, idxs, freqs = []
+    cont = True
+    count = 0
+    while ((str(count) + "_" + "docmat") in corpus):
+        docmats.append(corpus[str(count) + "_" + "docmat"])
+        idxs.append(corpus[str(count) + "_" + "index"])
+        freqs.append(corpus[str(count) + "_" + "freq"])
+        count += 1
+    ld_corpus = {
+        "docmats": docmats,
+        "idxs": idxs,
+        "freqs": freqs
+    }
+    return ld_corpus
